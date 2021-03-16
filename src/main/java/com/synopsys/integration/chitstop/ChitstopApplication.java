7b5ca980b712b0beec75fe5f6fb2018175747233
Eric Kerwin <ekerwin@synopsys.com>
2021-03-16 14:37:59 -0400

    initial commit

---
+++ src/main/java/com/synopsys/integration/chitstop/ChitstopApplication.java
@@ -0,0 +1,12 @@
+package com.synopsys.integration.chitstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitstopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChitstopApplication.class, args);
+	}
+
+}
